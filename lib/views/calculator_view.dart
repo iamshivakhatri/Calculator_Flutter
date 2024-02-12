f14159d587ed8f3d300f68576a3c86bde3febe7d
Shiva Khatri <shivakhatri@Shivas-Air.lan>
2024-02-11 21:25:39 -0500

    Completed the simple app

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -1,53 +1,111 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CalculatorView extends StatelessWidget {
+class CalculatorView extends StatefulWidget {
   const CalculatorView({super.key});
 
   @override
+  State<CalculatorView> createState() => _CalculatorViewState();
+}
+
+class _CalculatorViewState extends State<CalculatorView> {
+  int x = 0;
+  int y = 0;
+  num result = 0;
+
+  final displayOneController = TextEditingController();
+  final displayTwoController = TextEditingController();
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    displayOneController.text = x.toString();
+    displayTwoController.text = y.toString();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(12.0),
       child: Column(
         children: [
           // Calculator Display
-          DisplayOne(hint: "Enter a first number"),
-          SizedBox(
+          CalculatorDisplay(hint: "Enter a first number", controller: displayOneController),
+          const SizedBox(
             height: 20.0,
           ),  
-          DisplayOne(hint: "Enter a second number",),
-          SizedBox(
+          CalculatorDisplay(hint: "Enter a second number", controller: displayTwoController),
+          const SizedBox(
             height: 20.0,
           ),  
-          Text("0", 
-          style: TextStyle(
+          Text(
+             result.toString(),
+            style: const TextStyle(
             fontSize: 30.0,
         
           ),
           ),
 
-          Spacer(),
+          const Spacer(),
+            FloatingActionButton(
+              onPressed: (){
+                setState(() {
+                  displayOneController.text = "";
+                  displayTwoController.text = "";
+                  result = 0;
+                });
+              },
+              child: const Text("Clear")
+              ),
+              const Spacer(),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               FloatingActionButton(
-                onPressed: null,
-                child: Icon(CupertinoIcons.add),
+                onPressed: (){
+                  setState(() {
+                    x = int.parse(displayOneController.text);
+                    y = int.parse(displayTwoController.text);
+                    result = x + y;
+                  });
+                
+                },
+                child: const Icon(CupertinoIcons.add),
               ),
 
               FloatingActionButton(
-                onPressed: null,
-                child: Icon(CupertinoIcons.minus),
+                onPressed: (){
+                  setState(() {
+                    x = int.parse(displayOneController.text);
+                    y = int.parse(displayTwoController.text);
+                    result = x - y;
+                  });
+                },
+                child: const Icon(CupertinoIcons.minus),
               ),
 
               FloatingActionButton(
-                onPressed: null,
-                child: Icon(CupertinoIcons.multiply),
+                onPressed: (){
+                  setState((){
+                    x = int.parse(displayOneController.text);
+                    y = int.parse(displayTwoController.text);
+                    result = x * y;
+                  });
+                },
+                child: const Icon(CupertinoIcons.multiply),
               ),
 
               FloatingActionButton(
-                onPressed: null,
-                child: Icon(CupertinoIcons.divide),
+                onPressed: (){
+                  setState((){
+                    x = int.parse(displayOneController.text);
+                    y = int.parse(displayTwoController.text);
+                    result = x / y;
+                  });
+                },
+                child: const Icon(CupertinoIcons.divide),
               )
             ]
           )
@@ -58,17 +116,20 @@ class CalculatorView extends StatelessWidget {
   }
 }
 
-class DisplayOne extends StatelessWidget {
-  const DisplayOne({
+class CalculatorDisplay extends StatelessWidget {
+  const CalculatorDisplay({
     super.key,
     this.hint = "Enter a number",
+    required this.controller,
   });
   
   final String? hint;
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       keyboardType: TextInputType.number,
       autofocus: true,
       decoration: InputDecoration(
